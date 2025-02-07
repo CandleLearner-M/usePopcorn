42e13fc7ef7efbae3e24cc7d12dff3f9c9784da4
Mostafa <candlelearner.m.dev@gmail.com>
2025-02-07 10:12:56 +0100

    Creating a Star Component

--- src/StarRating.tsx
+++ src/StarRating.tsx
@@ -1,6 +1,98 @@
-function StarRating () {
+import { faStar } from "@fortawesome/free-regular-svg-icons";
+import { faStar as faStarFilled } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
+import { CSSProperties } from "react";
+
+const containerStyle = {
+  display: "flex",
+  alignItems: "center",
+  gap: "15px",
+};
+
+const startContainerStyle = {
+  display: "flex",
+  gap: "5px",
+};
+
+const textStyle = {
+  lineHeight: "1",
+  margin: "0",
+};
+
+interface StarRatingProps {
+  maxRating: number;
+}
+
+function StarRating({ maxRating = 5 }: StarRatingProps) {
+  const [rating, setRating] = useState(0);
+  const [hoverRating, setHoverRating] = useState(0);
+
+  const handleHover = function (num: number) {
+    setHoverRating(num);
+  };
+
   return (
-    <h1>Hello</h1>
+    <div style={containerStyle}>
+      <div style={startContainerStyle}>
+        {Array.from({ length: maxRating }, (_, index) => (
+          <Star
+            key={index}
+            onRate={() => setRating(index + 1)}
+            onHoverIn={() => handleHover(index + 1)}
+            onHoverOut={() => handleHover(0)}
+            isFilled={
+              hoverRating ? hoverRating >= index + 1 : index + 1 <= rating
+            }
+          />
+        ))}
+      </div>
+      <p style={textStyle}>{hoverRating || rating || ""}</p>
+    </div>
+  );
+}
+
+const starStyle = {
+  position: "relative",
+  color: "gold",
+  fontSize: "25px",
+  width: "25px",
+  height: "25px",
+  display: "block",
+  cursor: "pointer",
+} satisfies CSSProperties;
+
+const iconStyle = {
+  position: "absolute",
+  top: "0",
+  left: "0",
+  transition: "opacity 0.1s ease",
+} satisfies CSSProperties;
+
+interface StarProps {
+  onRate: () => void;
+  isFilled: boolean;
+  onHoverIn: () => void;
+  onHoverOut: () => void;
+}
+
+function Star({ onRate, isFilled, onHoverIn, onHoverOut }: StarProps) {
+  return (
+    <span
+      style={starStyle}
+      onClick={onRate}
+      onMouseEnter={onHoverIn}
+      onMouseLeave={onHoverOut}
+    >
+      <FontAwesomeIcon
+        icon={faStar}
+        style={{ ...iconStyle, opacity: isFilled ? 0 : 1 }}
+      />
+      <FontAwesomeIcon
+        icon={faStarFilled}
+        style={{ ...iconStyle, opacity: isFilled ? 1 : 0 }}
+      />
+    </span>
   );
 }
 
