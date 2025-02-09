86a67da4de5970cc6d120f0484e6cfaec7c506a9
Mostafa <candlelearner.m.dev@gmail.com>
2025-02-09 16:02:16 +0100

    Making the App work Again

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import TextExpander from "./TextExpander";
-// import './index.css'
-// import App from './App.tsx'
+// import TextExpander from "./TextExpander";
+import './index.css'
+import App from './App.tsx'
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    {/* <App /> */}
-    <TextExpander visibleWordsCount={10} isExpanded={true} buttonColor="pink">
+    <App />
+    {/* <TextExpander visibleWordsCount={10} isExpanded={true} buttonColor="pink">
       Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni itaque libero, laborum natus expedita perferendis numquam molestias facilis a illo minima delectus ducimus nostrum ex odio ad! Quia, placeat quis. adipisicing elit. Magni itaque libero, laborum natus expedita perferendis numquam molestias facilis a illo minima delectus ducimus nostrum ex odio ad! Quia, placeat quis. adipisicing elit. Magni itaque libero, laborum natus expedita perferendis numquam molestias facilis a illo minima delectus ducimus nostrum ex odio ad! Quia, placeat quis. adipisicing elit. Magni itaque libero, laborum natus expedita perferendis numquam molestias facilis a illo minima delectus ducimus nostrum ex odio ad! Quia, placeat quis.
-    </TextExpander>
+    </TextExpander> */}
   </StrictMode>
 );
