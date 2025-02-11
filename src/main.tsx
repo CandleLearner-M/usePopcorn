0c1f7d79df28efbc6a04fea8455c54fe89717b53
Mostafa <candlelearner.m.dev@gmail.com>
2025-02-11 21:27:17 +0100

    adding the app

--- src/main.tsx
+++ src/main.tsx
@@ -7,8 +7,6 @@ import App from './App.tsx'
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-    {/* <TextExpander visibleWordsCount={10} isExpanded={true} buttonColor="pink">
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni itaque libero, laborum natus expedita perferendis numquam molestias facilis a illo minima delectus ducimus nostrum ex odio ad! Quia, placeat quis. adipisicing elit. Magni itaque libero, laborum natus expedita perferendis numquam molestias facilis a illo minima delectus ducimus nostrum ex odio ad! Quia, placeat quis. adipisicing elit. Magni itaque libero, laborum natus expedita perferendis numquam molestias facilis a illo minima delectus ducimus nostrum ex odio ad! Quia, placeat quis. adipisicing elit. Magni itaque libero, laborum natus expedita perferendis numquam molestias facilis a illo minima delectus ducimus nostrum ex odio ad! Quia, placeat quis.
-    </TextExpander> */}
+   
   </StrictMode>
 );
