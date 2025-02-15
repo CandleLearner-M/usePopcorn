c4fbad842a964503750eff364808261694360a09
Mostafa <candlelearner.m.dev@gmail.com>
2025-02-15 09:29:36 +0100

    Working on the title of the page

--- src/components/main-components/MovieDetails.tsx
+++ src/components/main-components/MovieDetails.tsx
@@ -1,162 +1,172 @@
-  import { useEffect, useState } from "react";
-  import { KEY } from "../common/KEY";
-  import LoadingSpinner from "../common/LoadingSpinner";
-  import StarRating from "../common/StarRating";
-  import IconButton from "../common/Buttons";
-  import { Movie } from "../common/types";
+import { useEffect, useState } from "react";
+import { KEY } from "../common/KEY";
+import LoadingSpinner from "../common/LoadingSpinner";
+import StarRating from "../common/StarRating";
+import IconButton from "../common/Buttons";
+import { Movie } from "../common/types";
 
-  type MovieDetailsProps = {
-    selectedMovie: string;
-    onClose: () => void;
-    onAddWatchedMovie: (movie: Movie) => void;
-    onRemoveWatchedMovie: (id: string) => void;
-    movieIsAdded: (id: string | null) => Movie | undefined;
-  };
+type MovieDetailsProps = {
+  selectedMovie: string;
+  onClose: () => void;
+  onAddWatchedMovie: (movie: Movie) => void;
+  onRemoveWatchedMovie: (id: string) => void;
+  movieIsAdded: (id: string | null) => Movie | undefined;
+};
 
-  interface MovieDetails {
-    Title: string;
-    Year: string;
-    Released: string;
-    Runtime: string;
-    Genre: string;
-    Director: string;
-    Actors: string;
-    Plot: string;
-    Poster: string;
-    imdbRating: string;
-    imdbID: string;
-    Ratings: Array<{
-      Source: string;
-      Value: string;
-    }>;
-  }
-  export default function MovieDetails({
-    selectedMovie,
-    onClose,
-    onAddWatchedMovie,
-    onRemoveWatchedMovie,
-    movieIsAdded,
-  }: MovieDetailsProps) {
-    const tempMovie = movieIsAdded(selectedMovie);
-    const isAdded = !!tempMovie;
-    const userRate = tempMovie?.userRating || 0;
+interface MovieDetails {
+  Title: string;
+  Year: string;
+  Released: string;
+  Runtime: string;
+  Genre: string;
+  Director: string;
+  Actors: string;
+  Plot: string;
+  Poster: string;
+  imdbRating: string;
+  imdbID: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+}
+export default function MovieDetails({
+  selectedMovie,
+  onClose,
+  onAddWatchedMovie,
+  onRemoveWatchedMovie,
+  movieIsAdded,
+}: MovieDetailsProps) {
+  const tempMovie = movieIsAdded(selectedMovie);
+  const isAdded = !!tempMovie;
+  const userRate = tempMovie?.userRating || 0;
 
-    const [movie, setMovie] = useState<MovieDetails | null>();
-    const [isLoading, setIsLoading] = useState(false);
-    const [rating, setRating] = useState(userRate);
+  const [movie, setMovie] = useState<MovieDetails | null>();
+  const [isLoading, setIsLoading] = useState(false);
+  const [rating, setRating] = useState(userRate);
 
-    useEffect(() => {
-      const currentMovie = movieIsAdded(selectedMovie);
-      setRating(currentMovie?.userRating || 0);
-    }, [selectedMovie, movieIsAdded]);
+  useEffect(() => {
+    const currentMovie = movieIsAdded(selectedMovie);
+    setRating(currentMovie?.userRating || 0);
+  }, [selectedMovie, movieIsAdded]);
 
-    useEffect(() => {
-      setIsLoading(true);
-      async function getMovie() {
-        try {
-          if (!selectedMovie) return;
-          const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedMovie}`
-          );
-          const data = await res.json();
-          setMovie(data);
-        } catch (error) {
-          console.error(error);
-        } finally {
-          setIsLoading(false);
-        }
+  useEffect(() => {
+    setIsLoading(true);
+    async function getMovie() {
+      try {
+        if (!selectedMovie) return;
+        const res = await fetch(
+          `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedMovie}`
+        );
+        const data = await res.json();
+        setMovie(data);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsLoading(false);
       }
-      getMovie();
-    }, [selectedMovie]);
+    }
+    getMovie();
+  }, [selectedMovie]);
 
-    if (!movie) return null;
-    const {
-      Poster: poster,
-      Genre: genre,
-      imdbRating,
-      Title: title,
-      Runtime: runtime,
-      Released: released,
-      Plot: plot,
-      Actors: actors,
-      Director: director,
-      Year,
-    } = movie;
-
-    const handleBtnAdd = function () {
-      onAddWatchedMovie({
-        Title: title,
-        imdbID: selectedMovie,
-        Poster: poster,
-        Year,
-        imdbRating: parseFloat(imdbRating) || 0,
-        runtime,
-        userRating: rating,
-      });
-      onClose();
+  useEffect(() => {
+    document.title = selectedMovie
+      ? "Movie: " + movie?.Title || "usePopcorn"
+      : "usePopcorn";
+    return () => {
+      document.title = "usePopcorn";
     };
-    return isLoading ? (
-      <LoadingSpinner>Loading...</LoadingSpinner>
-    ) : (
-      <>
-        <div className="poster-details">
-          <IconButton type="arrow" className="back-arrow" onClick={onClose} />
-          <div className="poster-title">
-            <img src={poster} alt={title} />
-            <div className="title-details">
-              <h2>{title}</h2>
+  }, [movie, selectedMovie]);
 
-              <div>
-                <p>{released}</p>
-                <p>&bull;</p>
-                <p>{runtime}</p>
-              </div>
-              <div>
-                <p>{genre}</p>
-              </div>
-              <div>
-                <p>⭐ {imdbRating} IMDB Rating</p>
-              </div>
-            </div>
-          </div>
+  if (!movie) return null;
 
-          <div className="star-about">
-            <div className="star">
-              <StarRating
-                maxRating={10}
-                size={20}
-                setUserRating={setRating}
-                defaultRating={rating}
-              />
+  const {
+    Poster: poster,
+    Genre: genre,
+    imdbRating,
+    Title: title,
+    Runtime: runtime,
+    Released: released,
+    Plot: plot,
+    Actors: actors,
+    Director: director,
+    Year,
+  } = movie;
 
-              {isAdded && (
-                <button
-                  className="btn-add-remove"
-                  onClick={() => {
-                    onRemoveWatchedMovie(selectedMovie);
-                    onClose();
-                  }}
-                >
-                  Remove from watch list
-                </button>
-              )}
+  const handleBtnAdd = function () {
+    onAddWatchedMovie({
+      Title: title,
+      imdbID: selectedMovie,
+      Poster: poster,
+      Year,
+      imdbRating: parseFloat(imdbRating) || 0,
+      runtime,
+      userRating: rating,
+    });
+    onClose();
+  };
+  return isLoading ? (
+    <LoadingSpinner>Loading...</LoadingSpinner>
+  ) : (
+    <>
+      <div className="poster-details">
+        <IconButton type="arrow" className="back-arrow" onClick={onClose} />
+        <div className="poster-title">
+          <img src={poster} alt={title} />
+          <div className="title-details">
+            <h2>{title}</h2>
 
-              {!isAdded && rating > 0 && (
-                <button className="btn-add-remove" onClick={handleBtnAdd}>
-                  + Add to list
-                </button>
-              )}
-            </div>
- 
             <div>
-              <p>
-                <em>{plot}</em>
-              </p>
-              <p>Starring {actors}</p>
-              <p>Directed by {director}</p>
+              <p>{released}</p>
+              <p>&bull;</p>
+              <p>{runtime}</p>
+            </div>
+            <div>
+              <p>{genre}</p>
+            </div>
+            <div>
+              <p>⭐ {imdbRating} IMDB Rating</p>
             </div>
           </div>
         </div>
-      </>
-    );
-  }
+
+        <div className="star-about">
+          <div className="star">
+            <StarRating
+              maxRating={10}
+              size={20}
+              setUserRating={setRating}
+              defaultRating={rating}
+            />
+
+            {isAdded && (
+              <button
+                className="btn-add-remove"
+                onClick={() => {
+                  onRemoveWatchedMovie(selectedMovie);
+                  onClose();
+                }}
+              >
+                Remove from watch list
+              </button>
+            )}
+
+            {!isAdded && rating > 0 && (
+              <button className="btn-add-remove" onClick={handleBtnAdd}>
+                + Add to list
+              </button>
+            )}
+          </div>
+
+          <div>
+            <p>
+              <em>{plot}</em>
+            </p>
+            <p>Starring {actors}</p>
+            <p>Directed by {director}</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
