f3cf7ea3792f6e16d8c95bc088980c8ae609294b
Mostafa <candlelearner.m.dev@gmail.com>
2025-02-17 17:23:43 +0100

    Making the app back to its normal state

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,11 @@ import { createRoot } from "react-dom/client";
 // import TextExpander from "./TextExpander";
 import './styles/index.css'
 import App from './App.tsx'
-import CurrencyConverter from "./CurrencyConverter.tsx";
+// import CurrencyConverter from "./CurrencyConverter.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    {/* <App /> */}
-   <CurrencyConverter />
+    <App />
+   {/* <CurrencyConverter /> */}
   </StrictMode>
 );
